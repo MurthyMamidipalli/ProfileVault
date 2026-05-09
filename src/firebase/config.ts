7c2a871683762718dbb295034c7f8a31b13520da
RamaChandra Murthy Mamidipalli <ramachandramurthymamidipalli@gmail.com>
2026-05-09 15:33:07 +0000

    i have already enabled email

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,15 @@
 
+/**
+ * Firebase Configuration
+ * 
+ * To get your configuration:
+ * 1. Go to the Firebase Console (https://console.firebase.google.com/)
+ * 2. Select your project.
+ * 3. Click the gear icon (Project Settings) > General.
+ * 4. Scroll down to "Your apps" and select your Web app (or create one).
+ * 5. Copy the 'firebaseConfig' object and paste the values below.
+ */
+
 export const firebaseConfig = {
   apiKey: "PLACEHOLDER",
   authDomain: "PLACEHOLDER",
